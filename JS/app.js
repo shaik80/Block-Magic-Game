5eaca856fc124547a414dde5c8c3dbbcfdc4a402
Jashwanthi928 <54614090+Jashwanthi928@users.noreply.github.com>
2019-09-19 14:09:27 +0530

    Drawing the square

--- JS/app.js
+++ JS/app.js
@@ -5,3 +5,11 @@ const col=column=10;
 const sq=squareSize= 20;
 //color of empty square
 const vacant="white";
+//draw a square
+function drawSquare(x,y,color){
+    c.fillStyle=color;
+    c.fillRect(x*sq,y*sq,sq,sq);
+
+    c.strokeStyle="black";
+    c.strokeRect(x*sq,y*sq,sq,sq);
+}
