d4687c2592b441ae6b1c11ccb423041865043e83
Shaik Mudassir <sh.mudassir98@gmail.com>
2019-09-19 01:30:21 -0700

    app.js

--- JS/app.js
+++ JS/app.js
@@ -3,5 +3,7 @@ const c=canvas.getContext('2d');
 const row=20;
 const col=column=10;
 const sq=squareSize= 20;
-//color of empty square
-const vacant="white";
+let grd = ctx.createLinearGradient(500,0,700,700);
+          grd.addColorStop(0,"#8bcbf1");
+          grd.addColorStop(1,"white");
+const VACANT = grd; // color of an empty square
