90942ca0649652395bd17c73c33384ff8bc7e0c5
Shaik Mudassir <sh.mudassir98@gmail.com>
2019-09-21 14:42:33 +0530

    background

--- JS/background.js
+++ JS/background.js
@@ -11,76 +11,86 @@ const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
 let gravity = 0.3
 let friction = 0.6
 
-addEventListener('resize', () => {
-    canvas.width = innerWidth
-    canvas.height = innerHeight
-
-    init()
-})
 
 function randomIntFromRange(min,max){
     return Math.floor(Math.random() * (max - min +1)+min)
 }
-function randomColor(colors){
-    return colors(Math.floor(Math.random()*color.length))
+function randomColor(){
+    return "#"+Math.floor(Math.random()*16777216).toString(16)
 }
-// Objects
-function squre(x, y, dy, radius, color) {
-    this.x = x
-    this.y = y
-    this.dy = dy
-    this.radius = radius
-    this.color = color
-}
-
-Ball.prototype.draw = function() {
-    ctx.beginPath()
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    ctx.fillStyle = this.color
-    ctx.fill()
-    ctx.stroke()
-    ctx.closePath()
-}
-
-Ball.prototype.update = function() {
-    if(this.y +this.radius > canvas.height){
-        this.dy = -this.dy * friction
+class Background{
+    constructor(x, y, dy, radius, color) {
+        this.x = x
+        this.y = y
+        this.dy = dy
+        this.radius = radius
+        this.color = color
     }
-    else{
-        this.dy += gravity
-    }
-    this.y +=this.dy
 
-    this.draw()
+    draw(){
+    
+    
+    
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+        ctx.stroke()
+        ctx.closePath()
+    }
+    
+    update() {
+
+        if(this.y +this.radius > canvas.height){
+            this.dy = -this.dy * friction
+        }
+        else{
+            this.dy += gravity
+        }
+        this.y +=this.dy
+    
+        this.draw()
+    }
+
+
+
+    
 }
+
+
+
 
 // Implementation
 let ball;
 let ballarray = []
 
 function init() {
-    ball = new squre(canvas.width/2,canvas.height/2,1,30,"red")
-
+    ball = new Background()
     for (let i = 0; i < 100; i++) {
         let x =randomIntFromRange(0, canvas.width)
         let Y =randomIntFromRange(0, canvas.height - 20)
-        ballarray.push(new squre(x,Y,2,30,"red"))
+        ballarray.push(new Background(x,Y,1,20,randomColor()))
     }
 }
 
 // Animation Loop
 function animate() {
-    requestAnimationFrame(animate)
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.font = "30px Arial";
-// ctx.fillText("Hello World",canvas.width/2, canvas.height/2);
-// ctx.fillStyle = "#FF0000";
-// ctx.fillRect(0,0,200,55)
     ballarray.forEach(ball => {
-     ball.update()
+     ball.draw()
     })
-    ball.update()
+    ball.draw()
 }
 
 init()
-animate()
+$("#btn-play").click(
+    function animate() {
+        requestAnimationFrame(animate)
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        ctx.font = "30px Arial";
+        ballarray.forEach(ball => {
+         ball.update()
+        })
+        ball.update()
+    }
+)
+animate();
